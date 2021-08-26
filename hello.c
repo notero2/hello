09fa0df487f0e8e1c3d2e3c43ea5ac4d731428b8
notero2 <notero2@huskers.unl.edu>
2021-08-26 19:22:24 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Nicholas Otero
+ * Date: 2021/08/26
+ * 
+ * A simple hello world program in c
+ * /
+ * /
+ * //
+ * */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **agrv) {
+  
+  printf("notero2\n");
+  
+  return 0;
+ }
+ 
